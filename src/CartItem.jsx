0f0f073d90fe29e795d07e47388684288740c325
DonnyDaniel <donny@outlook.es>
2025-07-20 14:24:35 -0500

    Proyecto Final: Aplicación de Compras Paradise Nursery

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,62 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
+
+    const parseItemCostToInteger = (itemCost) => {
+        //Eliminar el símbolo de moneda antes de la multiplicación.De lo contrario, se devuelve NaN.
+        return parseInt(itemCost.replace('$', ''), 10);
+    };
+
+
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+         let totalCost = 0;
+
+        cart.forEach((item) => {
+            const itemCost = parseItemCostToInteger(item.cost);
+            totalCost += itemCost * item.quantity;
+        });
+
+        return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+        onContinueShopping(e);
   };
 
-
+    const handleCheckoutShopping = (e) => {
+        alert('Funcionalidad en mantenimiento.');
+    };
 
   const handleIncrement = (item) => {
+          const updatedItem = { ...item };
+          updatedItem.quantity++;
+          dispatch(updateQuantity(updatedItem));
   };
 
   const handleDecrement = (item) => {
-   
+          const updatedItem = { ...item };
+  
+          if (updatedItem.quantity == 1) {
+              // Eliminar elemento si el número de elementos disminuye a 0.
+              dispatch(removeItem(updatedItem));
+          } else {
+              updatedItem.quantity--;
+              dispatch(updateQuantity(updatedItem));
+          }
   };
 
   const handleRemove = (item) => {
+        dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+        let totalCost = 0;
+        const itemCost = parseItemCostToInteger(item.cost);
+        totalCost = item.quantity * itemCost;
+
+        return totalCost;
   };
 
   return (
@@ -57,12 +90,10 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
